bd0df029aed6bbf8dc2d50ea05b08bb4333149bb
Ebenezr <ebenezarbukosia@gmail.com>
2023-01-25 18:06:00 +0300

    [ADD] add footer and header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="h-20 w-full">
+      <div className="align-center flex h-full w-full  px-6">
+        <button className="">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="h-6 w-7 text-primary"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+            />
+          </svg>
+        </button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
