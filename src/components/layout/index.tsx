cc19e1c1e20918b0b27f4ed08ba886a45f8dc2d4
Ebenezr <ebenezarbukosia@gmail.com>
2023-01-30 13:07:52 +0300

    [ADD] add product details UI style

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import styles from "../../styles/Home.module.css";
@@ -7,8 +7,27 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+      const [isMobile, setIsMobile] = useState(false);
+      const [isIpad, setIsIpad] = useState(false);
+      const [isDesktop, setIsDesktop] = useState(false);
+
+      useEffect(() => {
+        setIsMobile(/iPhone|Android/i.test(navigator.userAgent));
+        setIsIpad(/iPad/i.test(navigator.userAgent));
+        setIsDesktop(/Windows|Mac|Linux/i.test(navigator.userAgent));
+      }, []);
   return (
-    <main className={`bg-bg ${styles.mobileContainer}`}>
+    <main
+      className={`  bg-bg ${
+        isMobile
+          ? styles.mobileContainer
+          : isIpad
+          ? styles.ipadContainer
+          : isDesktop
+          ? styles.desktopContainer
+          : styles.mobileContainer
+      }`}
+    >
       <Header />
       {children}
       <Footer />
--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,3 +1,150 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import Rating from "@material-ui/lab/Rating";
+import { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@mui/material/Button";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+
+const useStyles = makeStyles({
+  icon: {
+    height: "24px",
+  },
+});
 export default function () {
-  return <section className="flex  flex-1 flex-col">products</section>;
+  const [isReadMore, setIsReadMore] = useState(true);
+  const [value, setValue] = useState<number | null>(2);
+  const classes = useStyles();
+  const toggleReadMore = () => {
+    setIsReadMore(!isReadMore);
+  };
+
+  return (
+    <section className="flex flex-1 flex-col px-6">
+      {/* product image */}
+      {/* wrapper */}
+      <div className="mt-4">
+        <Carousel
+          // autoPlay={true}
+          centerMode={false}
+          emulateTouch={true}
+          infiniteLoop={true}
+          stopOnHover={true}
+          centerSlidePercentage={100}
+          interval={5000}
+          transitionTime={1000}
+          showStatus={false}
+          showArrows={false}
+          useKeyboardArrows={true}
+          className=" mx-auto flex h-48 w-2/3 flex-col"
+        >
+          {/* product images */}
+          <div className=" h-44 w-full bg-green-500"></div>
+          <div className=" h-44 w-full bg-green-200"></div>
+        </Carousel>
+      </div>
+      {/* prod feat */}
+      <div className="flex w-full flex-1 flex-col ">
+        <p className="text-lg font-bold">Noodle</p>
+        <div className="mt-3 flex items-center gap-2">
+          <p className="">4.0</p>
+          {/* star rating */}
+          <span className="flex">
+            <Rating
+              name="read-only"
+              value={value}
+              precision={0.5}
+              readOnly
+              icon={
+                <span className={classes.icon}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="h-5 w-5"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                    />
+                  </svg>
+                </span>
+              }
+            />
+          </span>
+          <small className="text-neutral-400">(89 reviews)</small>
+        </div>
+
+        <p className="mt-2">Size: 500G</p>
+        <p className="mt-3 text-lg font-bold text-primary">KES 55.00</p>
+        <p className="mt-4 text-lg font-semibold">Descriptions</p>
+        <p className="mt-3 leading-5 text-neutral-700">
+          {isReadMore ? prodDesc.slice(0, 150) : prodDesc}
+          <button
+            onClick={toggleReadMore}
+            className="flex w-full cursor-pointer justify-center gap-2 py-4  text-[16px] font-medium leading-4 tracking-wide text-primary "
+          >
+            {isReadMore ? (
+              <>
+                Show more
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="h-6 w-6 text-primary"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                  />
+                </svg>
+              </>
+            ) : (
+              <>
+                Show less
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="h-6 w-6 text-primary"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M4.5 15.75l7.5-7.5 7.5 7.5"
+                    />
+                  </svg>
+                </span>
+              </>
+            )}
+          </button>
+        </p>
+      </div>
+      {/* buttons */}
+      <div className="grid w-full grid-cols-2 gap-3 py-8">
+        <Button variant="outlined" startIcon={<AddShoppingCartIcon />}>
+          Add to cart
+        </Button>
+        <Button
+          color="primary"
+          variant="contained"
+          style={{ backgroundColor: "#010F8C", color: "#fff" }}
+        >
+          Buy Now
+        </Button>
+      </div>
+    </section>
+  );
 }
+
+const prodDesc =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, quasi Lorem ipsum dolor sit amet consectetur adipisicing elit.Vel, recusandae ad praesentium quod culpa nemo Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, quasi Lorem ipsum dolor sit amet consectetur adipisicing elit.Vel, recusandae ad praesentium quod culpa nemo";
