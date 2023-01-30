cbb48711613f4e807b01f8d98c7881112aa05345
Ebenezr <ebenezarbukosia@gmail.com>
2023-01-30 13:07:08 +0300

    [ADD] add material ui theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,23 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
 import styles from "../styles/Home.module.css";
 import Layout from "../components/layout";
-
+const theme = createTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: "#010F8C",
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+  },
+});
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
@@ -15,11 +25,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <>
       <SessionProvider session={session}>
-        {/*  */}
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-        {/* </main> */}
+        <ThemeProvider theme={theme}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ThemeProvider>
       </SessionProvider>
     </>
   );
