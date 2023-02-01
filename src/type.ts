f399425acb58c7e6ed990f89d439fc40b04df4f3
Ebenezr <ebenezarbukosia@gmail.com>
2023-02-01 15:23:09 +0300

    [ADD] define types to products schema

---
+++ src/type.ts
@@ -0,0 +1,50 @@
+export type Product = {
+  id: number;
+  name: string;
+  description: string | null;
+  price: number;
+  image_url: string | null;
+  size: string;
+  reviews: [] | null;
+  categoryId: number;
+  discountId: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type ProductCategory = {
+  id: number;
+  name: string;
+  products: [] | null;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type ProductReview = {
+  id: number;
+  productId: number;
+  rating: number;
+  decs: string;
+  reviewId: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type ProductInventory = {
+  id: number;
+  quantity: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type ProductDiscount = {
+  id: number;
+  name: string;
+  percentage_discount: number;
+  active: boolean;
+  code: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+// user
